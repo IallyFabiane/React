e61ed6cec76f6e564b8f578e110280d17c584cdf
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-05-24 19:26:31 -0300

    Add files via upload

---
+++ lista-tarefas/src/components/Main.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './Main.css';
+
+export default class Main extends Component {
+    state = { // class fields JS
+      novaTarefa: '',
+    }; //estado do componente
+
+
+  handleChange = (e) => {
+    this.setState( {
+      novaTarefa: e.target.value
+    });
+  }
+
+  render() {
+
+    const { novaTarefa } = this.state;
+
+    return(
+      <div className='main'>
+         <h1>{novaTarefa}</h1>
+         <form action='#'>
+      <input onChange={this.handleChange} type='text'></input>
+      <button type='submit'>Enviar</button>
+          </form>
+      </div>
+    );
+  }
+}
