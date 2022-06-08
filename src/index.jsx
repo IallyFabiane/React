e31ca8fab60d45daa1ffee1bff4c6f56d330fee7
Ially Fabiane <iallyfabiane@gmail.com>
2022-06-08 12:44:43 -0300

    passos iniciais com react

---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import { render } from 'react-dom';
+import { App } from './App';
+
+render(<App />, document.getElementById('root')) 
+//primeiro parâmetro: o que eu quero renderizar, segundo parâmetro: dentro de qual elemento eu quero renderizar
