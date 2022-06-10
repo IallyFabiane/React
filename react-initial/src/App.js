8691c0291d175b46b5b077af815bf938baab4c98
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-06-10 16:29:51 -0300

    Update App.js
    Adicionando array de objetosdentro do state

--- react-initial/src/App.js
+++ react-initial/src/App.js
@@ -1,49 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
 class App extends Component { //componente react. Para escrever um código JavaScript em JSX usamos um par de chaves {} e escrevemos o códgio dentro
-  constructor(props) { //oconstructor no React recebe props
-    super(props);
-    this.handlePClick = this.handlePClick.bind(this); //handlePClick agora pode acessar this
-    this.state = {  //criando o estado para a classe. O estado corresponde a um objeto que contém os dados do componente que serão renderizados a partir da função render()
-      name: 'Ially Fabiane',
-      counter: 0
-    };
-  }
+    //é possível criar um componente de estado sem utilizar o constructor
+  state = {  //criando o estado para a classe. O estado corresponde a um objeto que contém os dados do componente que serão renderizados a partir da função render()
+      posts: [ //array de objetos em jsx: utilizamos : e não o sinal = para declarar um array
+        {
+          id: 1,
+          title: 'título 1',
+          body: 'corpo 1'
+        },
+        {
+          id: 2,
+          title: 'título 2',
+          body: 'corpo 2'
+        },
+        {
+          id: 3,
+          title: 'título 3',
+          body: 'corpo 3'
+        }
+      ]
+  };
 
-  handlePClick() { //modificando o estado do componente de classe App após a chamada do método handlePClick()
-      this.setState({name: 'Full Stack Developer Junior'})
-  }
+render() {
+//só podemos ter um componente root dentro da página com React. Para adicionar mais um componente, devemos colocá-lo dentro do root
+//precisamos inserir um Key com uma propriedade dentro do componente-pai quando estamos utilizando o método .map()
+    const { posts } = this.state;
 
-  handleAClick = (event) => { //acesssando o this do componente de classe App sem utilizar o método bind()
-    event.preventDefault();
-    const { counter } = this.state;
-    this.setState({counter: counter + 1})
-  }
-
-  render() {
-    const { name, counter } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" /> 
-          <p onClick={this.handlePClick}>
-            {name} {counter}
-          </p>
-          <a onClick={this.handleAClick}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Clique aqui:
-          </a>
-        </header>
+      <div className='App'>
+        {posts.map(post => (
+          <div key={post.id}> 
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
       </div>
     );
   }
-}
-  
+} 
 
 export default App;
