171fde89e7114db2f4b20bdf54519241e56cc6af
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-06-10 14:47:43 -0300

    Update App.js
    Atualizando o componente de classe App

--- react-initial/src/App.js
+++ react-initial/src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() { //componente react. Para escrever um código JavaScript em JSX usamos um par de chaves {} e escrevemos o códgio dentro
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" /> 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component { //componente react. Para escrever um código JavaScript em JSX usamos um par de chaves {} e escrevemos o códgio dentro
+  constructor(props) { //oconstructor no React recebe props
+    super(props);
+    this.handlePClick = this.handlePClick.bind(this); //handlePClick agora pode acessar this
+    this.state = {  //criando o estado para a classe. O estado corresponde a um objeto que contém os dados do componente que serão renderizados a partir da função render()
+      name: 'Ially Fabiane',
+      counter: 0
+    };
+  }
+
+  handlePClick() { //modificando o estado do componente de classe App após a chamada do método handlePClick()
+      this.setState({name: 'Full Stack Developer Junior'})
+  }
+
+  handleAClick = (event) => { //acesssando o this do componente de classe App sem utilizar o método bind()
+    event.preventDefault();
+    const { counter } = this.state;
+    this.setState({counter: counter + 1})
+  }
+
+  render() {
+    const { name, counter } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" /> 
+          <p onClick={this.handlePClick}>
+            {name} {counter}
+          </p>
+          <a onClick={this.handleAClick}
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Clique aqui:
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
+  
 
 export default App;
