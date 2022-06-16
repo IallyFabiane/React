dfe801082fdf8e8ad2b296bd354bc1487702cea2
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-06-16 16:07:03 -0300

    Update App.js

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -1,8 +1,16 @@
-import React from 'react'; 
+import React from 'react';
+import { ListaDeNotas } from './components/ListaDeNotas'
 
 function App() {
   return (
-    <h1>Meu App React</h1>
+    <section>
+       <form>
+        <input type="text" placeholder="Título"/>
+        <textarea placeholder='Escreva sua nota...'></textarea>
+       <button>Criar Nota</button>
+    </form>
+   <ListaDeNotas />
+    </section>
   );
 }
 
