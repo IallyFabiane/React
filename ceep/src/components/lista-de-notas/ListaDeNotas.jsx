c861ddbcb925008c88499edabb8018a7279ae49b
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-06-17 15:59:27 -0300

    Update ListaDeNotas.jsx

--- ceep/src/components/lista-de-notas/ListaDeNotas.jsx
+++ ceep/src/components/lista-de-notas/ListaDeNotas.jsx
@@ -6,11 +6,11 @@ export class ListaDeNotas extends Component {
    render() {
        return (
        <ul className='lista-notas'>
-         {Array.of("Trabalho", "Trabalho", "Estudos").map((categoria, index)=> {
+         {this.props.notas.map((nota, index)=> {
            return (
             <li className='lista-notas_item' key={index}>
-            <div>{categoria}</div>
-            <CardNota />
+            <div>{nota}</div>
+            <CardNota titulo={nota.titulo} texto={nota.texto} />
            </li>
            );
          })}
@@ -18,5 +18,3 @@ export class ListaDeNotas extends Component {
       );
    } 
 }
-
-//O método Array.of() cria um nova instância de Array com um número variável de argumentos, independentemente do número ou do tipo dos argumentos.
