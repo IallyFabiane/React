a935e1be0468d9377996c50b4966d709308059d9
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-06-16 21:45:26 -0300

    Update ListaDeNotas.jsx
    substituindo o conteúdo das tags li por um componente (CardNota)

--- ceep/src/components/ListaDeNotas.jsx
+++ ceep/src/components/ListaDeNotas.jsx
@@ -1,33 +1,19 @@
 import React, { Component } from 'react';
+import CardNota from './CardNota';
 
 export class ListaDeNotas extends Component {
    render() {
        return (
        <ul>
         <li>
-          <section>
-            <header>
-              <h3>Título</h3>
-            </header>
-            <p>Escreva sua nota</p>
-          </section>
-          </li>
-          <li>
-          <section>
-            <header>
-              <h3>Título</h3>
-            </header>
-            <p>Escreva sua nota</p>
-          </section>
-          </li>
-          <li>
-          <section>
-            <header>
-              <h3>Título</h3>
-            </header>
-            <p>Escreva sua nota</p>
-          </section>
-          </li>
+          <CardNota />
+        </li>
+        <li>
+         <CardNota />
+        </li>
+         <li>
+         <CardNota /> 
+        </li>
       </ul>
       );
    } 
