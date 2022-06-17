65f0b728f85af6bd3a8855376056c17f8201f2ee
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-06-16 22:02:17 -0300

    Update ListaDeNotas.jsx

--- ceep/src/components/ListaDeNotas.jsx
+++ ceep/src/components/ListaDeNotas.jsx
@@ -4,17 +4,18 @@ import CardNota from './CardNota';
 export class ListaDeNotas extends Component {
    render() {
        return (
-       <ul>
-        <li>
-          <CardNota />
-        </li>
-        <li>
-         <CardNota />
-        </li>
-         <li>
-         <CardNota /> 
-        </li>
-      </ul>
+         <ul>
+            {Array.of("Trabalho", "Trabalho", "Estudos").map(categoria => {
+              return (
+               <li>
+               <div>{categoria}</div>
+               <CardNota />
+              </li>
+              );
+            })}
+         </ul>
       );
    } 
 }
+//O método Array.of() cria um nova instância de Array com um número variável de argumentos, independentemente do número ou do tipo dos argumentos.
+//o método de array .map() retorna uma lista contendo o conteúdo das tags li
