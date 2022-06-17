6bc29f5c4cf9d123624624b4de8f405fd7c25ad4
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-06-17 11:53:27 -0300

    Update ListaDeNotas.jsx

--- ceep/src/components/lista-de-notas/ListaDeNotas.jsx
+++ ceep/src/components/lista-de-notas/ListaDeNotas.jsx
@@ -5,10 +5,10 @@ import "./estilo.css";
 export class ListaDeNotas extends Component {
    render() {
        return (
-       <ul>
+       <ul className='lista-notas'>
          {Array.of("Trabalho", "Trabalho", "Estudos").map((categoria, index)=> {
            return (
-            <li key={index}>
+            <li className='lista-notas_item' key={index}>
             <div>{categoria}</div>
             <CardNota />
            </li>
