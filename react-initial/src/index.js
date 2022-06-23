8b2ec662050ca281ebfa673f34a326f204514984
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-06-23 15:04:37 -0300

    Update index.js

--- react-initial/src/index.js
+++ react-initial/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import '../src/components/styles/global-styles.css';
+import Home from './templates/Home';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 );
 
