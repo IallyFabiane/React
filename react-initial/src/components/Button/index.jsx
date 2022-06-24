e708692c1bb340cb1009ad3805fa2c8df89e6f62
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-06-24 12:16:01 -0300

    Update index.jsx

--- react-initial/src/components/Button/index.jsx
+++ react-initial/src/components/Button/index.jsx
@@ -1,8 +1,9 @@
 import { Component } from 'react'
+import './styles.css'
 export class Button extends Component {
 
     render() {
-        const { text, onClick } = this.props;
-      return  (<button onClick={onClick}>{text}</button>)
+        const { text, onClick, disabled } = this.props;
+      return  (<button disabled={disabled} className='button' onClick={onClick}>{text}</button>)
     }
 }
