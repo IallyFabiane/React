d941c3de6a87ae219bca19d1170429daa014dd7d
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-06-28 14:02:49 -0300

    Delete react-initial directory

--- react-initial/src/components/Button/index.jsx
+++
@@ -1,9 +0,0 @@
-import { Component } from 'react'
-import './styles.css'
-export class Button extends Component {
-
-    render() {
-        const { text, onClick, disabled } = this.props;
-      return  (<button disabled={disabled} className='button' onClick={onClick}>{text}</button>)
-    }
-}
