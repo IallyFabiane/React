db0fec7f0c5402a4ac1775eae9d89e15ca498f2b
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-06-30 20:01:34 -0300

    Update App.js

--- formulario_de_cadastro/src/App.js
+++ formulario_de_cadastro/src/App.js
@@ -1,15 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component, useContext } from 'react'
 import './App.css';
 import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
 import { Container, Typography } from '@mui/material'
 import 'fontsource-roboto';
+import { validarCpf, validarSenha } from './models/cadastro';
+import ValidacoesCadastro from './context/ValidacoesCadastro'
 
 class App extends Component {
   render() {
     return (
       <Container component='article' maxwidth='lg'>
         <Typography variant='h3' component='h1' align='center'>Formulário de Cadastro</Typography>
-        <FormularioCadastro aoEnviar={aoEnviarForm} validarCpf={validarCpf} />  
+        <ValidacoesCadastro.Provider
+          value={{ cpf: validarCpf, senha: validarSenha, nome: validarSenha }}
+        >
+          <FormularioCadastro aoEnviar={aoEnviarForm} />
+        </ValidacoesCadastro.Provider>
       </Container>
     );
   }
@@ -19,13 +25,5 @@ function aoEnviarForm (dados) {
   console.log(dados)
 }
 
-
-function validarCpf(cpf) {
-  if(cpf.length !== 11) {
-    return { valido:false, texto: "O CPF deve ter 11 dígitos"}
-  } else {
-    return {valido: true, texto: ""}
-  }
-}
 
 export default App;
