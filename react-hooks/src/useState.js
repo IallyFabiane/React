0da22f5a3d9e27dd964001142b2fc160f6c9a750
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-07-09 21:09:28 -0300

    Add files via upload

---
+++ react-hooks/src/useState.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './useState.css';
+import { useState } from 'react';
+
+function App() {
+  const [reverse, setReverse] = useState(false); //recebe o primeiro estado
+  const [counter, setCounter] = useState(0);
+  const reverseClass = reverse ? 'reverse' : '';
+
+  const handleClick = () => {
+    setReverse(!reverse); //recebe o segundo estado
+  };
+
+  const handleIncrement = () => {
+    setCounter(counter + 1);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
+
+        <h1>Contador: {counter}</h1>
+
+        <p>
+          <button type="button" onClick={handleClick}>
+            Reverse {reverseClass}
+          </button>
+        </p>
+        <p>
+          <button type="button" onClick={handleIncrement}>
+            Increment {counter}
+          </button>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
