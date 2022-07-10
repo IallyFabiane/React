454a913e2a1263d9d4855d5c60bbe896ce48cf1c
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-07-09 21:01:19 -0300

    Update index.jsx

--- react-initial/src/components/Button/index.jsx
+++ react-initial/src/components/Button/index.jsx
@@ -1,9 +1,25 @@
-import { Component } from 'react'
-import './styles.css'
-export class Button extends Component {
+import { Component } from 'react';
+import './styles.css';
+import { proptypes } from 'prop-types';
 
-    render() {
-        const { text, onClick, disabled } = this.props;
-      return  (<button disabled={disabled} className='button' onClick={onClick}>{text}</button>)
-    }
+export class Button extends Component {
+  render() {
+    const { text, onClick, disabled } = this.props;
+    return (
+      // eslint-disable-next-line react/react-in-jsx-scope
+      <button disabled={disabled} className="button" onClick={onClick}>
+        {text}
+      </button>
+    );
+  }
 }
+
+Button.defaultProps = {
+  disabled: false,
+};
+
+Button.propTypes = {
+  text: proptypes.string,
+  onClick: proptypes.func.isRequired,
+  disabled: proptypes.bool,
+};
