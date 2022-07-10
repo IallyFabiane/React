9d994dffcb085e110ca5e7d00f2bde7db3d086b4
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-07-09 21:06:05 -0300

    Update index.jsx

--- react-initial/src/components/TextInput/index.jsx
+++ react-initial/src/components/TextInput/index.jsx
@@ -1,13 +1,20 @@
+/* eslint-disable react/react-in-jsx-scope */
 import './styles.css';
+import { proptypes } from 'prop-types';
 
-export const TextInput = ({ searchValue, actionFn}) => {
-   return (
-   <input 
-        className='text-input'
-        type="search" 
-        value={searchValue}
-        onChange={actionFn}
-        placeholder="Type your search"
+export const TextInput = ({ searchValue, actionFn }) => {
+  return (
+    <input
+      className="text-input"
+      type="search"
+      value={searchValue}
+      onChange={actionFn}
+      placeholder="Type your search"
     />
-    );
-}
+  );
+};
+
+TextInput.propTypes = {
+  searchValue: proptypes.string.isRequired,
+  actionFn: proptypes.func.isRequired,
+};
