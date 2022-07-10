193e702ad7f3b25ace33e35a146c80522c90ba58
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-07-09 21:03:02 -0300

    Update index.jsx

--- react-initial/src/components/PostCard/index.jsx
+++ react-initial/src/components/PostCard/index.jsx
@@ -1,10 +1,19 @@
+/* eslint-disable react/react-in-jsx-scope */
+import { proptypes } from 'prop-types';
+
 export const PostCard = ({ title, cover, body, id }) => (
-    <div className='post'>
-      <img src={cover} alt= {title}/>
-     <div className= 'post-content'> 
-        <h1>{title}</h1>
-        <p>{body}</p>
-      </div>
+  <div className="post" key={id}>
+    <img src={cover} alt={title} />
+    <div className="post-content">
+      <h1>{title}</h1>
+      <p>{body}</p>
     </div>
+  </div>
 );
 
+PostCard.propTypes = {
+  title: proptypes.string,
+  cover: proptypes.string,
+  body: proptypes.string,
+  id: proptypes.number,
+};
