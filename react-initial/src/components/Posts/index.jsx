61a66f9943b23da41fdf211d7446900e7493c15a
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-07-09 21:04:52 -0300

    Update index.jsx

--- react-initial/src/components/Posts/index.jsx
+++ react-initial/src/components/Posts/index.jsx
@@ -1,15 +1,26 @@
+/* eslint-disable react/react-in-jsx-scope */
 import { PostCard } from '../PostCard/';
-import './styles.css'
-export const Posts = ({posts}) => {
-    <div className='posts'>
-        {posts.map(post => (
-          <PostCard 
-          key={post.id}
-          title={post.title} 
-          body={post.body}
-          id={post.id}
-          cover={post.cover}
-          />
-        ))}
-    </div>
-}
+import './styles.css';
+import { proptypes } from 'prop-types';
+
+export const Posts = ({ posts = [] }) => {
+  <div className="posts">
+    {posts.map((post) => (
+      <PostCard
+        key={post.id}
+        title={post.title}
+        body={post.body}
+        id={post.id}
+        cover={post.cover}
+      />
+    ))}
+  </div>;
+};
+
+Posts.defaultProps = {
+  posts: [],
+};
+
+Posts.propTypes = {
+  posts: proptypes.array,
+};
