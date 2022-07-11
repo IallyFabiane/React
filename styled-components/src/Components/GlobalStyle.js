e78c28dd260294c9fc1ddc2c1b49201b60116b44
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-07-11 13:38:00 -0300

    Add files via upload

---
+++ styled-components/src/Components/GlobalStyle.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components'; //criando um arquivo de reset css global
+
+export const GolbalStyle = createGlobalStyle `
+* {
+  box-sizing: border-box;
+  font-family: "Montserrat", sans-serif;
+  margin: 0;
+  padding: 0;
+  text-decoration: none;
+}
+`;
---
+++ styled-components/src/Components/UI/temas.js
@@ -0,0 +1,17 @@
+import { fundoClaro, fundoEscuro, conteudoClaro, conteudoEscuro, textoFundoClaro, textoFundoEscuro } from './variaveis';
+
+export const temaClaro = 
+{
+  body: fundoClaro,
+  inside: conteudoClaro,
+  text: textoFundoClaro,
+  filter: ''
+};
+
+export const temaEscuro = 
+{
+  body: fundoEscuro,
+  inside: conteudoEscuro,
+  text: textoFundoEscuro,
+  filter: 'invert(100)'
+}
