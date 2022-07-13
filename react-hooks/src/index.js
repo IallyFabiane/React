c654e1e6c9cd1b50904836f3d39f9d588dc8c90a
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-07-13 16:45:53 -0300

    Update index.js

--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './useState';
+import UseEffect from './useEffect';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <UseEffect />
   </React.StrictMode>,
 );
 
