957c20e7d3ca505a37e068d08fa9ecb3e466c244
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-07-18 16:45:18 -0300

    Delete lista-tarefas directory

--- lista-tarefas/src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Main from './components/Main'
-import './App.css';
-
-export default function App() { // esta função retorna um códig similar ao HTML, chamado JSX.
-  return <Main />;
-}
-
