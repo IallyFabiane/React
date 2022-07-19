0f8c2b8cfe6f781a7aef9a038ae4e6c75916e859
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-07-19 15:57:12 -0300

    Update index.jsx

--- react-hooks(useContext)/src/components/Div/index.jsx
+++ react-hooks(useContext)/src/components/Div/index.jsx
@@ -1,7 +1,7 @@
 import { H1 } from '../H1';
 import { P } from '../P';
-// eslint-disable-next-line
-export const Div = ({ children }) => {
+
+export const Div = () => {
     return (
       <>
         <H1 />
